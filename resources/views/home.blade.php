41dd1f051960c97d49e1ea5a190d8a9a611beb05
dhiza <dhizahayati23@gmail.com>
2024-07-24 14:37:49 +0700

    test2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/app.css')
-  <title> Home </title>
+  <title> Welcome </title>
 </head>
 
 <body class="bg-gray-200 shadow flex flex-col min-h-screen">
